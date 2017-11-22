e4a84975b9c5e94039a85a1bb5ac9cf51cc6f223
Kristina Groeger <kristinagroeger@gmail.com>
2017-11-22 11:58:51 -0500

    explain it, okay?
    splain.

--- credit card mask/creditCardMask.js
+++ credit card mask/creditCardMask.js
@@ -1,7 +1,12 @@
 // return masked string
-
 function maskify(cc) {
+  
+// return first digit up until the last four digits  
 return cc.slice(0, -4)
+  
+// replace everything except the last four digits with a hash (#)  
 .replace(/./g,"#")
+
+// add back the last four digits after the hashes (#)
 + cc.slice(-4);
 }
