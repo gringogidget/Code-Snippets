7b0474e763adf7fbb6caa49bb9b9b69bdb5a2208
Kristina Groeger <kristinagroeger@gmail.com>
2017-11-22 11:55:05 -0500

    Even shorter / smaller

--- credit card mask/creditCardMask.js
+++ credit card mask/creditCardMask.js
@@ -1,12 +1,7 @@
 // return masked string
+
 function maskify(cc) {
-//
-    var x = cc.toString();
-// replace x with #
-    var y = x.replace(/.(?=.{4,}$)/g, '#');
-// check the length of the string and stop at 4
-    for (var i = (y.length - 4); i < y.length; i++) {
-        y[i] = x[i];
-    }
-    return y;
+return cc.slice(0, -4)
+.replace(/./g,"#")
++ cc.slice(-4);
 }
