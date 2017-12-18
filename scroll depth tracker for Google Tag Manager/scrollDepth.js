c085b70f182b1c218c43c93f7d6fd0d320433a0c
Kristina Groeger <kristinagroeger@gmail.com>
2017-12-18 16:29:57 -0500

    scroll depth tracker for Google Tag Manager
    Configure later to do more granular than 25%, 50%, 75%, 100%.

---
+++ scroll depth tracker for Google Tag Manager/scrollDepth.js
@@ -0,0 +1,368 @@
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+50
+51
+52
+53
+54
+55
+56
+57
+58
+59
+60
+61
+62
+63
+64
+65
+66
+67
+68
+69
+70
+71
+72
+73
+74
+75
+76
+77
+78
+79
+80
+81
+82
+83
+84
+85
+86
+87
+88
+89
+90
+91
+92
+93
+94
+95
+96
+97
+98
+99
+100
+101
+102
+103
+104
+105
+106
+107
+108
+109
+110
+111
+112
+113
+114
+115
+116
+117
+118
+119
+120
+121
+122
+123
+124
+125
+126
+127
+128
+129
+130
+131
+132
+133
+134
+135
+136
+137
+138
+139
+140
+141
+142
+143
+144
+145
+146
+147
+148
+149
+150
+151
+152
+153
+154
+155
+156
+157
+158
+159
+160
+161
+162
+163
+164
+165
+166
+167
+168
+169
+170
+171
+172
+173
+174
+175
+176
+177
+178
+179
+180
+181
+182
+183
+184
+<script>
+/*!
+ * @preserve
+ * jquery.scrolldepth.js | v0.4.1
+ * Copyright (c) 2014 Rob Flaherty (@robflaherty)
+ * Licensed under the MIT and GPL licenses.
+ */
+;(function ( $, window, document, undefined ) {
+
+  "use strict";
+
+  var defaults = {
+    elements: [],
+    minHeight: 0,
+    percentage: true,
+    testing: false
+  },
+
+  $window = $(window),
+  cache = [];
+
+  /*
+   * Plugin
+   */
+
+  $.scrollDepth = function(options) {
+
+    var startTime = +new Date;
+
+    options = $.extend({}, defaults, options);
+
+    // Return early if document height is too small
+    if ( $(document).height() < options.minHeight ) {
+      return;
+    }
+
+    // Establish baseline (0% scroll)
+    sendEvent('Percentage', 'Baseline');
+
+    /*
+     * Functions
+     */
+
+    function sendEvent(action, label, timing) {
+      if (!options.testing) {
+
+        if (typeof(dataLayer) !== "undefined") {
+          dataLayer.push({'event':'ScrollDistance', 'eventCategory':'Scroll Depth', 'eventAction': action, 'eventLabel': label, 'eventValue': 1, 'eventNonInteraction': true});
+
+          if (arguments.length > 2) {
+            dataLayer.push({'event':'ScrollTiming', 'eventCategory':'Scroll Depth', 'eventAction': action, 'eventLabel': label, 'eventTiming': timing});
+          }
+        } else {
+
+          if (typeof(ga) !== "undefined") {
+            ga('send', 'event', 'Scroll Depth', action, label, 1, {'nonInteraction': 1});
+
+            if (arguments.length > 2) {
+              ga('send', 'timing', 'Scroll Depth', action, timing, label);
+            }
+          }
+
+          if (typeof(_gaq) !== "undefined") {
+            _gaq.push(['_trackEvent', 'Scroll Depth', action, label, 1, true]);
+
+            if (arguments.length > 2) {
+              _gaq.push(['_trackTiming', 'Scroll Depth', action, timing, label, 100]);
+            }
+          }
+        }
+
+      } else {
+        $('#console').html(action + ': ' + label);
+      }
+    }
+
+    function calculateMarks(docHeight) {
+      return {
+        '25%' : parseInt(docHeight * 0.25, 10),
+        '50%' : parseInt(docHeight * 0.50, 10),
+        '75%' : parseInt(docHeight * 0.75, 10),
+        // 1px cushion to trigger 100% event in iOS
+        '100%': docHeight - 1
+      };
+    }
+
+    function checkMarks(marks, scrollDistance, timing) {
+      // Check each active mark
+      $.each(marks, function(key, val) {
+        if ( $.inArray(key, cache) === -1 && scrollDistance >= val ) {
+          sendEvent('Percentage', key, timing);
+          cache.push(key);
+        }
+      });
+    }
+
+    function checkElements(elements, scrollDistance, timing) {
+      $.each(elements, function(index, elem) {
+        if ( $.inArray(elem, cache) === -1 && $(elem).length ) {
+          if ( scrollDistance >= $(elem).offset().top ) {
+            sendEvent('Elements', elem, timing);
+            cache.push(elem);
+          }
+        }
+      });
+    }
+
+    /*
+     * Throttle function borrowed from:
+     * Underscore.js 1.5.2
+     * http://underscorejs.org
+     * (c) 2009-2013 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
+     * Underscore may be freely distributed under the MIT license.
+     */
+
+    function throttle(func, wait) {
+      var context, args, result;
+      var timeout = null;
+      var previous = 0;
+      var later = function() {
+        previous = new Date;
+        timeout = null;
+        result = func.apply(context, args);
+      };
+      return function() {
+        var now = new Date;
+        if (!previous) previous = now;
+        var remaining = wait - (now - previous);
+        context = this;
+        args = arguments;
+        if (remaining <= 0) {
+          clearTimeout(timeout);
+          timeout = null;
+          previous = now;
+          result = func.apply(context, args);
+        } else if (!timeout) {
+          timeout = setTimeout(later, remaining);
+        }
+        return result;
+      };
+    }
+
+    /*
+     * Scroll Event
+     */
+
+    $window.on('scroll.scrollDepth', throttle(function() {
+      /*
+       * We calculate document and window height on each scroll event to
+       * account for dynamic DOM changes.
+       */
+
+      var docHeight = $(document).height(),
+        winHeight = window.innerHeight ? window.innerHeight : $window.height(),
+        scrollDistance = $window.scrollTop() + winHeight,
+
+        // Recalculate percentage marks
+        marks = calculateMarks(docHeight),
+
+        // Timing
+        timing = +new Date - startTime;
+
+      // If all marks already hit, unbind scroll event
+      if (cache.length >= 4 + options.elements.length) {
+        $window.off('scroll.scrollDepth');
+        return;
+      }
+
+      // Check specified DOM elements
+      if (options.elements) {
+        checkElements(options.elements, scrollDistance, timing);
+      }
+
+      // Check standard marks
+      if (options.percentage) {
+        checkMarks(marks, scrollDistance, timing);
+      }
+    }, 500));
+
+  };
+
+})( jQuery, window, document );
+jQuery.scrollDepth();
+</script>
